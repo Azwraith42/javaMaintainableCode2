b4fbd9a0838b0ad12b03941ed362e325cfb6f1eb
Reeves <areeves@WL-AREEVES-MAC.local>
2017-07-14 13:08:32 -0700

    typo

--- src/main/java/org.cj.alec.maintainableCode2/RouteLength.java
+++ src/main/java/org.cj.alec.maintainableCode2/RouteLength.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 public class RouteLength implements Route {
     @Override
     public ResponseTuple getResponseTuple(Optional<String> maybeTarget) {
-        return maybeTarget.map((target) -> new ResponseTuple(HttpServletResponse.SC_OK, String.format("Length: %d!", target.length()))).orElseGet(
+        return maybeTarget.map((target) -> new ResponseTuple(HttpServletResponse.SC_OK, String.format("Length: %d", target.length()))).orElseGet(
                 () -> new ResponseTuple(HttpServletResponse.SC_BAD_REQUEST, "Bad Request")
         );
     }
